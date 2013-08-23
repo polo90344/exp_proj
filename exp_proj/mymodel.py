2f8849aba8901feca3093dd7a722ffcc770c006a
polo90344 <polo90344@163.com>
2013-08-23 15:13:55 +0800

    project created

---
+++ exp_proj/mymodel.py
@@ -0,0 +1,63 @@
+import win32api as kernel32
+import win32con as con
+from ctypes import *
+
+
+
+#进制转换函数
+def anyto10(str, any=16):
+    t = '0123456789ABCDEF'
+    lstr = len(str)
+    ustr = str.upper()
+    ret = 0
+    i = 0
+    while lstr:
+        ret = ret * any + t.find(ustr[i])
+        i += 1
+        lstr -= 1
+    return ret
+
+
+#内存操作
+def readMemoryIntType(pid, lpBaseAddress):
+    hProcess = kernel32.OpenProcess(con.PROCESS_ALL_ACCESS, 0, pid)
+    lpBuffer = c_int(0)
+    bytesRead = c_ulong(0)
+    ret = windll.kernel32.ReadProcessMemory(hProcess, lpBaseAddress, byref(lpBuffer), 4, byref(bytesRead))
+    windll.api.CloseHandl(hProcess)
+    if ret == 0:
+        return -1
+    return lpBuffer.value
+
+def readGameIntType(pid, base, offset1=None, offset2=None, offset3=None, offset4=None):
+    if not offset1:
+        return readMemoryIntType(pid, anyto10(base))
+    data = readMemoryIntType(pid, anyto10(base))
+    if not offset2:
+        return readMemoryIntType(pid, data + anyto10(offset1))
+    data = readMemoryIntType(pid, data + anyto10(offset1))
+    if not offset3:
+        return readMemoryIntType(pid, data + anyto10(offset2))
+    data = readMemoryIntType(pid, data + anyto10(offset2))
+    if not offset4:
+        return readMemoryIntType(pid, data + anyto10(offset3))
+    data = readMemoryIntType(pid, data + anyto10(offset3))
+    return readMemoryIntType(pid, data + anyto10(offset4))
+
+def writeMemoryIntType(pid, address, value):
+    hProcess = kernel32.OpenProcess(con.PROCESS_ALL_ACCESS, 0, pid)
+    value = c_int(value)
+    ret = windll.kernel32.WriteProcessMemory(hProcess, address, byref(value), 4, 0)
+    windll.api.CloseHandle(hProcess)
+    if ret == 0:
+        return False
+    return True
+    
+
+
+
+
+if __name__ == "__main__":
+    a = input("Enter a Hex Number to Convert:")
+    r = anyto10(a)
+    print("Decimal Value is:", r)
---
+++ exp_proj/plant_memory.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import mymodel as my
+import win32gui as gui
+import win32process as p
+
+
+
+hwnd = gui.FindWindowEx(None, None, "MainWindow", "ֲ植物大战僵尸中文版")
+
+pid = gui.getWindowPid(hwnd)
+
+print ("当前阳光数:", my.readGameIntType(pid, '6a9ec0', '768', '5560'))
+
+base = my.anyto10('6a9ec0')
+base1 = my.anyto10('768')
+base2 = my.anyto10('5560')
+
+
+offset1 = my.readMemoryIntType(pid, base) + base1
+address = my.readMemoryIntType(pid, offset1) + base2
+
+value = input('Please Enter Your Value > ')
+my.writeMemoryIntType(pid, address, int(value))
